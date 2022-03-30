c3e30ab21439a00894913bff58b7803ee6749130
Richard Blundell <none@none.com>
2022-03-30 08:19:48 +0000

    m

---
+++ tests/test_sshmodule2.c
@@ -0,0 +1,24 @@
+/*******************/
+#include <stdio.h>
+/*******************/
+#include "def.h"
+#include "module.h"
+#include "require.h"
+/*******************/
+#include "../include/sshmodule2.h"
+/*******************/
+#include "../../log/log.c"
+
+
+/*******************/
+
+
+int main(void) {
+  module(sshmodule2) * sshmodule2 = require(sshmodule2);
+  log_debug("sshmodule2:%d", sshmodule2);
+  log_debug("sshmodule2->secret:%d", sshmodule2->secret);
+  log_debug("sshmodule2->private_connect:%d", sshmodule2->private_connect());
+  log_debug("sshmodule2->secret:%d", sshmodule2->secret);
+  clib_module_free(sshmodule2);
+  return(0);
+}
